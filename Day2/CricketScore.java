e9a62c9b5e81ad0a0bc3297b3da989d963ef14f1
purimetlamuralikrishna <muralikrishna70321@gmail.com>
2022-05-17 21:08:35 +0530

    assig

--- Day2/CricketScore.java
+++ Day2/CricketScore.java
@@ -4,7 +4,13 @@ public class CricketScore {
 
 	public static void main(String[] args) {
 		
-		
+		int one_s = 1;
+		int two_s = 1;
+		int three_s = 1;
+		int four_s = 1;
+		int six_s = 1;
+		int total_score = one_s*1+two_s*2+three_s*3+four_s*4+six_s*6;
+		System.out.println(total_score);
 
 	}
 
